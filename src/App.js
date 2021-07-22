626661e5d8eb3c6e82ccdfe2719755c8287788da
Abdul Moiz <moiza0799@gmail.com>
2021-07-22 18:29:29 +0500

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+        <div className="container-fluid d-flex flex-column p-0">
+          <a
+            className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
+            href="#"
+          >
+            <div className="sidebar-brand-icon rotate-n-15">
+              <i className="fas fa-laugh-wink"></i>
+            </div>
+            <div className="sidebar-brand-text mx-3">
+              <span>Brand</span>
+            </div>
+          </a>
+          <ul className="navbar-nav text-light" id="accordionSidebar">
+            <li className="nav-item">
+              <a className="nav-link active" href="index.html">
+                <i className="fas fa-tachometer-alt"></i>
+                <span>Dashboard</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="profile.html">
+                <i className="fas fa-user"></i>
+                <span>Profile</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="table.html">
+                <i className="fas fa-table"></i>
+                <span>Table</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="login.html">
+                <i className="far fa-user-circle"></i>
+                <span>Login</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="register.html">
+                <i className="fas fa-user-circle"></i>
+                <span>Register</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="text-center d-none d-md-inline">
+          <button
+            className="btn rounded-circle border-0"
+            id="sidebarToggle"
+            type="button"
+          ></button>
+        </div>
+      </nav>
     </div>
   );
 }
